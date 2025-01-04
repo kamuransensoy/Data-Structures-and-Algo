2d6d38b0ae278077241bb6d3b4de67432079b7af
Selim Kamuran Sensoy <kamuransensoy@gmail.com>
2025-01-04 18:59:59 -0500

    Updated files and added Practice.java

--- 1.Data-Structures/1.Arrays-and-Strings/OneDimArrays.java
+++ 1.Data-Structures/1.Arrays-and-Strings/OneDimArrays.java
@@ -1,7 +1,13 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 public class OneDimArrays {
 
+    public static void main(String[] args) {
+        
+    }
 /**
  * An array is a basic data structure to store a collection of elements sequentially. 
  * But elements can be accessed randomly since each element in the array can be identified by an array index. 
@@ -12,7 +18,8 @@ public class OneDimArrays {
  * Therefore, A[0] = 6. Similarly, A[1] = 3, A[2] = 8 and so on.
  */
 
-    public static void main(String[] args) {
+
+    public static void LinearArrays(){
         // 1. Initialize
         int[] array = {1,2,3,4};
         int[] arr = new int[4];
@@ -24,12 +31,12 @@ public class OneDimArrays {
         // 4. Iterate all Elements
         // 1st Verstion
         for(int i = 0; i < array.length; i++){
-            System.out.print(", " + array[i]);
+            System.out.print(" " + array[i]);
         }
         System.out.println();
         // 2nd Version
         for(int item: array){
-            System.out.print(", " + item);
+            System.out.print(" " + item);
         }
         System.out.println();
         // 5 Modify Element
@@ -41,4 +48,53 @@ public class OneDimArrays {
             System.out.print(" " + array[i]);
         }
     }
+
+/**
+ * As we mentioned in the previous article, an array has a fixed capacity and we need to specify the size of the array when we initialize it. 
+ * Sometimes this will be somewhat inconvenient and wasteful.
+ * Therefore, most programming languages offer built-in dynamic array which is still a random access list data structure but with variable size.
+ * Dynamic arrays are resizable arrays. Unlike static arrays, they can grow or shrink during runtime.
+ * 
+ */
+
+ public static void DynamicArrays(){
+    
+            // 1. initialize
+        List<Integer> v0 = new ArrayList<>();
+        List<Integer> v1;                           // v1 == null
+        // 2. cast an array to a vector
+        Integer[] a = {0, 1, 2, 3, 4};
+        v1 = new ArrayList<>(Arrays.asList(a));
+        // 3. make a copy
+        List<Integer> v2 = v1;                      // another reference to v1
+        List<Integer> v3 = new ArrayList<>(v1);     // make an actual copy of v1
+        // 4. get length
+        System.out.println("The size of v1 is: " + v1.size());
+        // 5. access element
+        System.out.println("The first element in v1 is: " + v1.get(0));
+        // 6. iterate the vector
+        System.out.print("[Version 1] The contents of v1 are:");
+        for (int i = 0; i < v1.size(); ++i) {
+            System.out.print(" " + v1.get(i));
+        }
+        System.out.println();
+        System.out.print("[Version 2] The contents of v1 are:");
+        for (int item : v1) {
+            System.out.print(" " + item);
+        }
+        System.out.println();
+        // 7. modify element
+        v2.set(0, 5);       // modify v2 will actually modify v1
+        System.out.println("The first element in v1 is: " + v1.get(0));
+        v3.set(0, -1);
+        System.out.println("The first element in v1 is: " + v1.get(0));
+        // 8. sort
+        Collections.sort(v1);
+        // 9. add new element at the end of the vector
+        v1.add(-1);
+        v1.add(1, 6);
+        // 10. delete the last element
+        v1.remove(v1.size() - 1);
+
+ }
 }
---
+++ 1.Data-Structures/1.Arrays-and-Strings/Practice.java
@@ -0,0 +1,100 @@
+/**
+ * This file is designed to serve as a comprehensive resource for Java learners and developers to practice coding problems. 
+ * It includes a variety of coding challenges and their corresponding solutions, 
+ * covering key programming concepts, algorithms, and data structures.
+ */
+
+public class Practice{
+
+public static void FindPivotIndex(int[] nums){
+
+/**
+ * Given an array of integers nums, calculate the pivot index of this array.
+ * The pivot index is the index where the sum of all the numbers strictly to the left of 
+ * the index is equal to the sum of all the numbers strictly to the index's right.If the index is on the left edge of the array, 
+ * then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+ * Return the leftmost pivot index. If no such index exists, return -1.
+ */
+
+        int[] array = nums;
+        int sumRight = 0;
+        int sumLeft = 0;
+        int total = 0;
+        
+        for(int i = 0; i < array.length; i++){
+            total += array[i];
+        }
+        
+        for(int i=0; i < array.length; i++){
+            
+            sumRight = total - sumLeft - array[i];
+            
+            if(sumRight == sumLeft){
+                //return i;
+            }
+            
+            sumLeft += array[i];
+        }
+        
+        //return -1;
+
+}
+
+public static void LargestNumberAtLeastTwiceofOthers(int[] nums){
+/**
+ * You are given an integer array nums where the largest integer is unique. 
+ * Determine whether the largest element in the array is at least twice as much as every other number in the array. 
+ * If it is, return the index of the largest element, or return -1 otherwise.
+ */
+
+
+        int[] array = nums;
+        int largest = Integer.MIN_VALUE;
+        int index = 0;
+        int length = array.length;
+        int secondLargest = 0;
+        
+        for(int i = 0; i < length; i++){
+            
+            if(array[i] > largest){
+                secondLargest = largest;
+                largest = array[i];
+                index = i;
+            }else if(nums[i] > secondLargest){
+                secondLargest = nums[i];
+            }
+            
+        }
+        if(secondLargest*2 <= largest){
+            //return index;
+        }
+        
+        //return -1;
+}
+
+public static void PlusOne(int[] digits){
+/**
+ * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+ * The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+ * Increment the large integer by one and return the resulting array of digits.
+ */
+
+    int[] array = digits;
+        int length = array.length;
+        
+        for(int i = length -1; i >= 0; i--){
+            if(array[i] < 9){
+                array[i] += 1;
+                //return array;
+            }
+            
+            array[i] = 0;    
+        }
+        int[] nwArr = new int[length+1];
+        nwArr[0] = 1;
+            
+        //return nwArr;
+}
+
+
+}
